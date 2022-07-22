037673e687d6cd2c1108e2ca3b12279fca197a1d
PD <praveendaspd@gmail.com>
2022-07-22 20:58:25 +1000

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { WarningComponent } from './warning/warning.component';
-import { SuccessComponent } from './success/success.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    WarningComponent,
-    SuccessComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
